5e8517d9a80e7295c06cff868d5aea9f781ef8a8
Vidushan <kvidushan@hotmail.com>
2017-10-30 07:02:16 +0000

    added a id randomizer

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
 class Pokemon extends Component {
+
   render() {
-      return (
-        <div className="pokemon">
-          <div className="pokemon_wrapper">
-            <img src={require(`../assets/sprites/1.png`)} width={500} height={500}/>
-          </div>
+    let id = Math.floor(Math.random() * 100) + 1;
+    return (
+      <div className="pokemon">
+        <div className="pokemon_wrapper">
+          <img src={require(`../assets/sprites/${id}.png`)} width={500} height={500} alt="images of pokemon"/>
         </div>
-      );
+      </div>
+    );
   }
 }
 export default Pokemon;
